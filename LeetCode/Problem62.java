bc5bdb71c5a35f4303870b1d94a94ca918eb0913
Jaeyoung Shim <sjy4566@naver.com>
2019-12-04 18:47:07 +0900

    today algorithm

---
+++ LeetCode/Problem62.java
@@ -0,0 +1,23 @@
+
+public class Problem62 {
+	
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        dp[0][0] = 1;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(i == 0 && j == 0) continue;
+                int up = (i - 1 >= 0) ? dp[i-1][j] : 0;
+                int left = (j - 1 >= 0) ? dp[i][j-1] : 0;
+                dp[i][j] = up + left;
+            }
+        }
+        return dp[m-1][n-1];
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
