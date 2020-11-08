2bfe5f2b60e01e8bb48ca0f089fa20df93cddfd8
Jaeyoung Shim <sjy4566@naver.com>
2020-11-08 22:23:59 +0900

    today algorithm

--- LeetCode/Problem53.java
+++ LeetCode/Problem53.java
@@ -4,24 +4,13 @@ import java.util.Comparator;
 public class Problem53 {
 	
     public int maxSubArray(int[] nums) {
-    	int max_sum = -1;
-        int sum = 0;
-        int size = 1;
-        while(size < nums.length) {
-            for(int i = 0; i < nums.length; i++) {
-            	for(int j = i; j < j + size; j++) {
-            		if(j > nums.length - 1) {
-            			continue;
-            		}
-            		sum = sum + nums[j];
-            	}
-            	max_sum = Math.max(max_sum, sum);
-            	sum = 0;
-            }
-            size++;
-        }
-        ArrayList<String> list = new ArrayList<>();
-        return max_sum;
+    	int maxSum = nums[0];
+    	int maxUntilNow = nums[0];
+    	for(int i = 1; i < nums.length; i++) {
+    		maxUntilNow = Math.max(maxUntilNow + nums[i], nums[i]);
+    		maxSum = Math.max(maxSum, maxUntilNow);
+    	}
+        return maxSum;
     }
 
 	public static void main(String[] args) {
