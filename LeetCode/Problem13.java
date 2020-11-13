3226b008dad3e6a9549368dd3ffc32352e51357b
Jaeyoung Shim <sjy4566@naver.com>
2020-11-13 13:20:34 +0900

    today algorithm

--- LeetCode/Problem13.java
+++ LeetCode/Problem13.java
@@ -1,5 +1,34 @@
 
 public class Problem13 {
+	
+    public int romanToInt(String s) {
+        int num = 0;
+        int l = s.length();
+        int last = 1000;
+        for (int i = 0; i < l; i++){
+            int v = getValue(s.charAt(i));
+            if (v > last){
+                num = num + v - last * 2;
+            } 
+            else{
+                num = num + v;
+            }
+            last = v;
+        }
+        return num;
+    }
+    private int getValue(char c){
+        switch(c){
+            case 'I' : return 1;
+            case 'V' : return 5;
+            case 'X' : return 10;
+            case 'L' : return 50;
+            case 'C' : return 100;
+            case 'D' : return 500;
+            case 'M' : return 1000;
+            default : return 0;
+        }
+    }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
--- LeetCode/Problem217.java
+++ LeetCode/Problem217.java
@@ -1,5 +1,15 @@
+import java.util.HashSet;
 
 public class Problem217 {
+	
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int n : nums){
+            if(set.contains(n)) return true;
+            set.add(n);
+        }
+        return false;
+    }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
