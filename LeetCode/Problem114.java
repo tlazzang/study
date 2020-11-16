8c0d3db10f2f290dd4bc8f21d74ec2a9799296b5
Jaeyoung Shim <sjy4566@naver.com>
2020-11-16 15:45:41 +0900

    today algorithm

--- LeetCode/Problem114.java
+++ LeetCode/Problem114.java
@@ -4,36 +4,29 @@ import java.util.List;
 import javax.swing.tree.TreeNode;
 
 public class Problem114 {
-	
-	
-	  public class TreeNode {
-	      int val;
-	      TreeNode left;
-	      TreeNode right;
-	      TreeNode(int x) { val = x; }
-	  }
-	
-	class Solution {
-	    List<Integer> list = new ArrayList<>();
-	    
-	    public void flatten(TreeNode root) {
-	        if(root==null) return;
-	        preorder(root);
-	        TreeNode cur = root;
-	        for(int i=1;i<list.size();i++){
-	            cur.left = null;
-	            cur.right = new TreeNode(list.get(i));
-	            cur = cur.right;
-	        }
-	    }
-	    
-	    public void preorder(TreeNode root){
-	        if(root==null) return;
-	        list.add(root.val);
-	        preorder(root.left);
-	        preorder(root.right);
-	    }
-	}
+    public void flatten(TreeNode root) {
+        if(root == null) return;
+        TreeNode now = root;
+        while(true){
+            if(now.left != null){
+                TreeNode tmp = now.right;
+                now.right = now.left;
+                getTreeNodeBottomRight(now.left).right = tmp;
+                now.left = null;
+            }else{
+                if(now.right == null) break;
+                now = now.right;
+            }
+        }
+    }
+    
+    public TreeNode getTreeNodeBottomRight(TreeNode root){
+        if(root.right == null){
+            return root;
+        }else{
+            return getTreeNodeBottomRight(root.right);
+        }
+    }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
