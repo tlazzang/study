faca533195e7d1c1147782d3f7e86f837c6c6ef7
Jaeyoung Shim <sjy4566@naver.com>
2020-11-16 13:58:54 +0900

    today algorithm

--- LeetCode/Problem387.java
+++ LeetCode/Problem387.java
@@ -1,5 +1,16 @@
 
 public class Problem387 {
+	
+    public int firstUniqChar(String s) {
+        int[] frequency = new int[26];
+        for(char c : s.toCharArray()){
+            frequency[c - 'a']++;
+        }
+        for(int i = 0; i < s.length(); i++){
+            if(frequency[s.charAt(i) - 'a'] == 1) return i;
+        }
+        return -1;
+    }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
